a2f8643439b96e7ea79224e0dd22cd1b184dbd93
CatzenDo <375003790@qq.com>
2020-06-28 16:19:24 +0800

    20200628

---
+++ Py/hellovscode.py
@@ -0,0 +1,7 @@
+'''
+@Date: 2020-06-26 03:01:22
+@Author: Catzen
+@LastEditTime: 2020-06-26 03:01:24
+@LastEditors: Catzen
+@Descripttion:
+'''
---
+++ Web/HTML/01.js
@@ -0,0 +1,35 @@
+/*
+ * @Date: 2020-06-20 00:19:48
+ * @Author: Catzen
+ * @LastEditTime: 2020-06-28 15:33:05
+ * @LastEditors: Catzen
+ * @Descripttion:
+ */
+
+
+// let aa = document.getElementById('banner');
+
+
+console.log(typeof name);
+console.log(aa);
+/**
+ * @name: Catzen
+ * @param {type}
+ * @return:
+ */
+var aa = function () {
+    if (2 > 1) {
+        console.log(222);
+    } else {
+        console.log(111);
+        for (let index = 0; index < 12; index++) {
+            const element = 12;
+        }
+    }
+};
+
+
+console.log(aa);
+// 370421197007071530
+// 91370481MA3DK14U7D
+console.log(abc);
