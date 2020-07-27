429070ddfdd8980fc7aa0d0f3a7257e6314690a6
CatzenDo <375003790@qq.com>
2020-07-28 00:02:43 +0800

    0728

---
+++ Web/wCSS/js/index.js
@@ -0,0 +1,12 @@
+/**
+* index.js
+* @author catzenDo
+* @description 
+* @created 2020-07-27T10:38:34.305Z+08:00
+* @copyright None 
+* None
+* @last-modified 2020-07-27T11:28:31.847Z+08:00
+*/
+
+var car = document.hasFocus();
+console.log(car);
